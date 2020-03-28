1c7b7e77b396d979bd6fd1c26f9d34f0ddb044ba
UtkrishtSinha <52488138+UtkrishtSinha@users.noreply.github.com>
2020-03-28 18:29:39 +0530

    Added few more ids registration

--- gym_chess/__init__.py
+++ gym_chess/__init__.py
@@ -1,18 +1,43 @@
 from gym.envs.registration import register
 
-register(
-    id='chess-v0',
-    entry_point='gym_chess.envs:ChessEnv',
-)
+board = ['.'] * 64
 
 register(
     id='ai_white-v0',
     entry_point='gym_chess.envs:ChessEnv',
-    kwargs={'player': 1}
+    kwargs={'player': 1, 'state': board}
 )
 
 register(
     id='ai_black-v0',
     entry_point='gym_chess.envs:ChessEnv',
-    kwargs={'player': -1}
+    kwargs={'player': -1, 'state': board}
+)
+
+register(
+    id='Human-v0',
+    entry_point='gym_chess.envs:PlayerEnv'
+)
+
+register(
+    id='BotVsBot-v0',
+    entry_point='gym_chess.envs:ChessGameTwoPlayers'
+)
+
+register(
+    id='BotVsHuman-v0',
+    entry_point='gym_chess.envs:ChessGameTwoPlayers',
+    kwargs = {'bot_black': False}
+)
+
+register(
+    id='HumanVsBot-v0',
+    entry_point='gym_chess.envs:ChessGameTwoPlayers',
+    kwargs = {'bot_white': False}
+)
+
+register(
+    id='HumanVsHuman-v0',
+    entry_point='gym_chess.envs:ChessGameTwoPlayers',
+    kwargs = {'bot_white': False, 'bot_black': False}
 )
